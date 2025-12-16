ccb34cb76fc88bc496fcc8d973dbfa287a76fdb2
Mangloo69 <yadav69.swayam@gmail.com>
2025-12-16 14:36:51 +0545

    favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,6 +20,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.svg" sizes="any" />
+      </head>
       <body className={`${jetbrainsMono.variable} antialiased`}>
         <Providers>{children}</Providers>
       </body>
